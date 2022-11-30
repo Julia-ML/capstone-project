dfab33383dc1a5f5a072fcde5f74b956679ad9c6
Ethan Newlin <newlin5@gmail.com>
2022-11-29 21:08:55 -0500

    added project team and task models

---
+++ server/db/Team.js
@@ -0,0 +1,15 @@
+const conn = require('./conn');
+const { STRING, UUID, UUIDV4 } = conn.Sequelize;
+
+const Team = conn.define('team', {
+	id: {
+		type: UUID,
+		primaryKey: true,
+		defaultValue: UUIDV4,
+	},
+	name: {
+		type: STRING,
+	},
+});
+
+module.exports = Team;
