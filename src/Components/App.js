cf6a6d42901e25d8956989cedc4ee8cf92824c3e
Julia <juliam.sec@gmail.com>
2022-12-01 21:14:02 -0500

    landing page and nav

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,15 @@
 import React, { useEffect } from "react";
 import Home from "./Home";
 import Login from "./Login";
+import LandingPage from "./LandingPage";
 import { useSelector, useDispatch } from "react-redux";
 import { loginWithToken } from "../store";
-import { Link, Routes, Route } from "react-router-dom";
+import { Link, Routes, Route, useNavigate } from "react-router-dom";
 
 const App = () => {
   const { auth } = useSelector((state) => state);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   useEffect(() => {
     dispatch(loginWithToken());
   }, []);
@@ -19,24 +21,32 @@ const App = () => {
   // }, [auth]);
   return (
     <div>
-      <h1>Acme Shopping</h1>
-      {auth.id ? (
-        <Home />
-      ) : (
-        <div>
-          <Login />
-        </div>
-      )}
-      {!!auth.id && (
+      <h1 onClick={() => navigate("/")}>Daily Standup Replacer</h1>
         <div>
           <nav>
-            <Link to="/">Home</Link>
+            {auth.id && (
+                <div class="nav">
+                  <Link to="/dashboard">Dashboard</Link>
+                  <Link to="/tasks">Tasks</Link>
+                  <Link to="/projects">Projects</Link>
+                  <Link to="/teammates">My Teammates</Link>
+                  <Link to="/profile">Profile</Link>
+                </div>
+              )}
           </nav>
-          <Routes></Routes>
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/dashboard" element={<Home />} />
+            </Routes>
         </div>
-      )}
+
     </div>
   );
 };
 
 export default App;
+
+
+//24   {!!auth.id && (
+//33        )}
