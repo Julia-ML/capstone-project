e87308d3ccbf61398ed60fa46f413b14ee4b2c4e
Ethan Newlin <newlin5@gmail.com>
2022-12-08 20:35:42 -0500

    created route to fetch tasks for a team

--- server/app.js
+++ server/app.js
@@ -7,12 +7,13 @@ app.use("/dist", express.static(path.join(__dirname, "../dist")));
 app.use("/static", express.static(path.join(__dirname, "../static")));
 
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "../static/index.html"))
+	res.sendFile(path.join(__dirname, "../static/index.html"))
 );
 
 app.use("/api/auth", require("./api/auth"));
 app.use("/api/teams", require("./api/teams"));
 app.use("/api/users", require("./api/users"));
 app.use("/api/projects", require("./api/projects"));
+app.use("/api/tasks", require("./api/tasks"));
 
 module.exports = app;
