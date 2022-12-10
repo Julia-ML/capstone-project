25a455014c08dcd9ed22e78b675127c13fb3db72
Karen Li <karennli2016@gmail.com>
2022-12-10 07:23:40 +0000

    added teamId to task creation

--- src/Components/ProjectDetail.js
+++ src/Components/ProjectDetail.js
@@ -20,307 +20,308 @@ import Typography from "@mui/material/Typography";
 import Drawer from "@mui/material/Drawer";
 
 const ProjectDetail = () => {
-	const { projects } = useSelector((state) => state);
-	const { id } = useParams();
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
-	const [project, setProject] = useState({});
-	const [backlog, setBacklog] = useState([]);
-	const [todo, setTodo] = useState([]);
-	const [progress, setProgress] = useState([]);
-	const [done, setDone] = useState([]);
-	const [columns, setColumns] = useState([]);
-	const [open, setOpen] = useState(false);
-	const [disabled, setDisabled] = useState(true);
-	const [drawerOpen, setDrawerOpen] = useState(false);
-	const [drawerTask, setDrawerTask] = useState({});
-	const [newTask, setNewTask] = useState({
-		name: "",
-		description: "",
-		status: "To Do",
-		projectId: id,
-	});
+  const { projects, tasks, auth } = useSelector((state) => state);
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [project, setProject] = useState({});
+  const [backlog, setBacklog] = useState([]);
+  const [todo, setTodo] = useState([]);
+  const [progress, setProgress] = useState([]);
+  const [done, setDone] = useState([]);
+  const [columns, setColumns] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [drawerTask, setDrawerTask] = useState({});
+  const [newTask, setNewTask] = useState({
+    name: "",
+    description: "",
+    status: "To Do",
+    projectId: id,
+    teamId: auth.teamId,
+  });
 
-	useEffect(() => {
-		dispatch(fetchProjects()), dispatch(fetchUsers(), dispatch(fetchTasks()));
-	}, []);
+  useEffect(() => {
+    dispatch(fetchProjects()), dispatch(fetchUsers(), dispatch(fetchTasks()));
+  }, []);
 
-	useEffect(() => {
-		if (projects[0] !== undefined) {
-			//kept getting project undefined error, changing from projects.length to this seems to fix it??
-			const project = projects.find((project) => project.id === id);
-			if (project && project.tasks) {
-				setProject(project);
-				setBacklog(project.tasks.filter((task) => task.status === "Backlog"));
-				setTodo(project.tasks.filter((task) => task.status === "To Do"));
-				setProgress(
-					project.tasks.filter((task) => task.status === "In Progress")
-				);
-				setDone(project.tasks.filter((task) => task.status === "Done"));
-			} else {
-				setProject(project);
-			}
-		}
-	}, [projects]);
+  useEffect(() => {
+    if (projects[0] !== undefined) {
+      //kept getting project undefined error, changing from projects.length to this seems to fix it??
+      const project = projects.find((project) => project.id === id);
+      if (project && project.tasks) {
+        setProject(project);
+        setBacklog(project.tasks.filter((task) => task.status === "Backlog"));
+        setTodo(project.tasks.filter((task) => task.status === "To Do"));
+        setProgress(
+          project.tasks.filter((task) => task.status === "In Progress")
+        );
+        setDone(project.tasks.filter((task) => task.status === "Done"));
+      } else {
+        setProject(project);
+      }
+    }
+  }, [projects]);
 
-	useEffect(() => {
-		setColumns({
-			1: {
-				name: "Backlog",
-				tasks: backlog,
-			},
-			2: {
-				name: "To do",
-				tasks: todo,
-			},
-			3: {
-				name: "In Progress",
-				tasks: progress,
-			},
-			4: {
-				name: "Done",
-				items: done,
-			},
-		});
-	}, [project]);
+  useEffect(() => {
+    setColumns({
+      1: {
+        name: "Backlog",
+        tasks: backlog,
+      },
+      2: {
+        name: "To do",
+        tasks: todo,
+      },
+      3: {
+        name: "In Progress",
+        tasks: progress,
+      },
+      4: {
+        name: "Done",
+        items: done,
+      },
+    });
+  }, [project]);
 
-	const handleClickOpen = () => {
-		setOpen(true);
-	};
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-	const handleClose = () => {
-		setOpen(false);
-	};
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-	const onChange = (ev) => {
-		newTask.name === "" || newTask.description === ""
-			? setDisabled(true)
-			: setDisabled(false);
-		setNewTask({ ...newTask, [ev.target.name]: ev.target.value });
-	};
+  const onChange = (ev) => {
+    newTask.name === "" || newTask.description === ""
+      ? setDisabled(true)
+      : setDisabled(false);
+    setNewTask({ ...newTask, [ev.target.name]: ev.target.value });
+  };
 
-	const createNewTask = () => {
-		dispatch(createTask(newTask));
-		//doesn't show up in column after creating until you refresh?
-		if (newTask.status === "To Do") {
-			setTodo([...todo, newTask]);
-		}
-		if (newTask.status === "Backlog") {
-			setBacklog([...backlog, newTask]);
-		}
-		if (newTask.status === "In Progress") {
-			setProgress([...progress, newTask]);
-		}
-		setColumns({ ...columns });
-		setNewTask({ ...newTask, name: "", description: "", status: "To Do" });
-		handleClose();
-	};
+  const createNewTask = () => {
+    dispatch(createTask(newTask));
+    //doesn't show up in column after creating until you refresh?
+    if (newTask.status === "To Do") {
+      setTodo([...todo, newTask]);
+    }
+    if (newTask.status === "Backlog") {
+      setBacklog([...backlog, newTask]);
+    }
+    if (newTask.status === "In Progress") {
+      setProgress([...progress, newTask]);
+    }
+    setColumns({ ...columns });
+    setNewTask({ ...newTask, name: "", description: "", status: "To Do" });
+    handleClose();
+  };
 
-	const toggleDrawer = () => {
-		setDrawerOpen(false);
-	};
+  const toggleDrawer = () => {
+    setDrawerOpen(false);
+  };
 
-	const onDragEnd = (result, columns, setColumns) => {
-		if (!result.destination) return;
-		const { source, destination } = result;
-		if (source.droppableId !== destination.droppableId) {
-			const sourceColumn = columns[source.droppableId];
-			const destColumn = columns[destination.droppableId];
-			const sourceTasks = [...sourceColumn.tasks];
-			const destTasks = [...destColumn.tasks];
-			const [removed] = sourceTasks.splice(source.index, 1);
-			destTasks.splice(destination.index, 0, removed);
-			setColumns({
-				...columns,
-				[source.droppableId]: {
-					...sourceColumn,
-					tasks: sourceTasks,
-				},
-				[destination.droppableId]: {
-					...destColumn,
-					tasks: destTasks,
-				},
-			});
-		} else {
-			const column = columns[source.droppableId];
-			const copiedTasks = [...column.tasks];
-			const [removed] = copiedTasks.splice(source.index, 1);
-			copiedTasks.splice(destination.index, 0, removed);
-			setColumns({
-				...columns,
-				[source.droppableId]: {
-					...column,
-					tasks: copiedItems,
-				},
-			});
-		}
-	};
+  const onDragEnd = (result, columns, setColumns) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns[source.droppableId];
+      const destColumn = columns[destination.droppableId];
+      const sourceTasks = [...sourceColumn.tasks];
+      const destTasks = [...destColumn.tasks];
+      const [removed] = sourceTasks.splice(source.index, 1);
+      destTasks.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...sourceColumn,
+          tasks: sourceTasks,
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          tasks: destTasks,
+        },
+      });
+    } else {
+      const column = columns[source.droppableId];
+      const copiedTasks = [...column.tasks];
+      const [removed] = copiedTasks.splice(source.index, 1);
+      copiedTasks.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...column,
+          tasks: copiedItems,
+        },
+      });
+    }
+  };
 
-	return (
-		<div>
-			<br />
-			<Typography variant='h4' align='center'>
-				{project.name}
-			</Typography>
-			<Container
-				sx={{ display: "flex", justifyContent: "center", height: "100%" }}
-			>
-				<DragDropContext
-					onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
-				>
-					{Object.entries(columns).map(([columnId, column], index) => {
-						return (
-							<div
-								style={{
-									display: "flex",
-									flexDirection: "column",
-									alignItems: "center",
-								}}
-								key={columnId}
-							>
-								<h2>{column.name}</h2>
-								<div style={{ margin: 8 }}>
-									<Droppable droppableId={columnId} key={columnId}>
-										{(provided, snapshot) => {
-											return (
-												<div
-													{...provided.droppableProps}
-													ref={provided.innerRef}
-													style={{
-														background: snapshot.isDraggingOver
-															? "lightblue"
-															: "lightgrey",
-														padding: 4,
-														width: 250,
-														minHeight: 500,
-													}}
-												>
-													{!column.tasks ? (
-														<div></div>
-													) : (
-														column.tasks.map((task, index) => {
-															return (
-																<Draggable
-																	key={task.id}
-																	draggableId={task.id}
-																	index={index}
-																>
-																	{(provided, snapshot) => {
-																		return (
-																			<div
-																				ref={provided.innerRef}
-																				{...provided.draggableProps}
-																				{...provided.dragHandleProps}
-																				style={{
-																					userSelect: "none",
-																					padding: 16,
-																					margin: "0 0 8px 0",
-																					minHeight: "50px",
-																					backgroundColor: snapshot.isDragging
-																						? "#263B4A"
-																						: "#456C86",
-																					color: "white",
-																					...provided.draggableProps.style,
-																				}}
-																			>
-																				<Button
-																					onClick={() => {
-																						setDrawerTask(task);
-																						setDrawerOpen(true);
-																					}}
-																				>
-																					{task.name}
-																				</Button>
-																			</div>
-																		);
-																	}}
-																</Draggable>
-															);
-														})
-													)}
-													{provided.placeholder}
-												</div>
-											);
-										}}
-									</Droppable>
-								</div>
-								<Button variant='contained' onClick={handleClickOpen}>
-									+ Add a task
-								</Button>
-								<Dialog open={open} onClose={handleClose}>
-									<DialogTitle>New Task</DialogTitle>
-									<DialogContent>
-										<DialogContentText>
-											Input task name and description:
-										</DialogContentText>
-										<TextField
-											autoFocus
-											margin='dense'
-											id='name'
-											label='name'
-											name='name'
-											type='text'
-											fullWidth
-											variant='standard'
-											value={newTask.name}
-											onChange={onChange}
-										/>
-										<TextField
-											autoFocus
-											id='desc'
-											label='description'
-											name='description'
-											type='text'
-											fullWidth
-											variant='standard'
-											value={newTask.description}
-											onChange={onChange}
-											margin='normal'
-											multiline
-										/>
-										<Select
-											name='status'
-											value={newTask.status}
-											onChange={onChange}
-											label='status'
-										>
-											<MenuItem value={"To Do"}>To Do</MenuItem>
-											<MenuItem value={"In Progress"}>In Progress</MenuItem>
-											<MenuItem value={"Done"}>Done</MenuItem>
-											<MenuItem value={"Backlog"}>Backlog</MenuItem>
-										</Select>
-										<FormHelperText>Status</FormHelperText>
-									</DialogContent>
-									<DialogActions>
-										<Button onClick={handleClose}>Cancel</Button>
-										<Button disabled={disabled} onClick={createNewTask}>
-											Create
-										</Button>
-									</DialogActions>
-								</Dialog>
-							</div>
-						);
-					})}
-				</DragDropContext>
-			</Container>
-			<Drawer
-				anchor={"right"}
-				open={drawerOpen}
-				onClose={toggleDrawer}
-				PaperProps={{
-					sx: { width: "40%" },
-				}}
-			>
-				<Container>
-					<br />
-					<Typography variant='h6'>{drawerTask.name}</Typography>
-					<br />
-					<Typography paragraph={true}>{drawerTask.description}</Typography>
-					<Typography>Status: {drawerTask.status}</Typography>
-				</Container>
-			</Drawer>
-		</div>
-	);
+  return (
+    <div>
+      <br />
+      <Typography variant="h4" align="center">
+        {project.name}
+      </Typography>
+      <Container
+        sx={{ display: "flex", justifyContent: "center", height: "100%" }}
+      >
+        <DragDropContext
+          onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+        >
+          {Object.entries(columns).map(([columnId, column], index) => {
+            return (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+                key={columnId}
+              >
+                <h2>{column.name}</h2>
+                <div style={{ margin: 8 }}>
+                  <Droppable droppableId={columnId} key={columnId}>
+                    {(provided, snapshot) => {
+                      return (
+                        <div
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                          style={{
+                            background: snapshot.isDraggingOver
+                              ? "lightblue"
+                              : "lightgrey",
+                            padding: 4,
+                            width: 250,
+                            minHeight: 500,
+                          }}
+                        >
+                          {!column.tasks ? (
+                            <div></div>
+                          ) : (
+                            column.tasks.map((task, index) => {
+                              return (
+                                <Draggable
+                                  key={task.id}
+                                  draggableId={task.id}
+                                  index={index}
+                                >
+                                  {(provided, snapshot) => {
+                                    return (
+                                      <div
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                        style={{
+                                          userSelect: "none",
+                                          padding: 16,
+                                          margin: "0 0 8px 0",
+                                          minHeight: "50px",
+                                          backgroundColor: snapshot.isDragging
+                                            ? "#263B4A"
+                                            : "#456C86",
+                                          color: "white",
+                                          ...provided.draggableProps.style,
+                                        }}
+                                      >
+                                        <Button
+                                          onClick={() => {
+                                            setDrawerTask(task);
+                                            setDrawerOpen(true);
+                                          }}
+                                        >
+                                          {task.name}
+                                        </Button>
+                                      </div>
+                                    );
+                                  }}
+                                </Draggable>
+                              );
+                            })
+                          )}
+                          {provided.placeholder}
+                        </div>
+                      );
+                    }}
+                  </Droppable>
+                </div>
+                <Button variant="contained" onClick={handleClickOpen}>
+                  + Add a task
+                </Button>
+                <Dialog open={open} onClose={handleClose}>
+                  <DialogTitle>New Task</DialogTitle>
+                  <DialogContent>
+                    <DialogContentText>
+                      Input task name and description:
+                    </DialogContentText>
+                    <TextField
+                      autoFocus
+                      margin="dense"
+                      id="name"
+                      label="name"
+                      name="name"
+                      type="text"
+                      fullWidth
+                      variant="standard"
+                      value={newTask.name}
+                      onChange={onChange}
+                    />
+                    <TextField
+                      autoFocus
+                      id="desc"
+                      label="description"
+                      name="description"
+                      type="text"
+                      fullWidth
+                      variant="standard"
+                      value={newTask.description}
+                      onChange={onChange}
+                      margin="normal"
+                      multiline
+                    />
+                    <Select
+                      name="status"
+                      value={newTask.status}
+                      onChange={onChange}
+                      label="status"
+                    >
+                      <MenuItem value={"To Do"}>To Do</MenuItem>
+                      <MenuItem value={"In Progress"}>In Progress</MenuItem>
+                      <MenuItem value={"Done"}>Done</MenuItem>
+                      <MenuItem value={"Backlog"}>Backlog</MenuItem>
+                    </Select>
+                    <FormHelperText>Status</FormHelperText>
+                  </DialogContent>
+                  <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button disabled={disabled} onClick={createNewTask}>
+                      Create
+                    </Button>
+                  </DialogActions>
+                </Dialog>
+              </div>
+            );
+          })}
+        </DragDropContext>
+      </Container>
+      <Drawer
+        anchor={"right"}
+        open={drawerOpen}
+        onClose={toggleDrawer}
+        PaperProps={{
+          sx: { width: "40%" },
+        }}
+      >
+        <Container>
+          <br />
+          <Typography variant="h6">{drawerTask.name}</Typography>
+          <br />
+          <Typography paragraph={true}>{drawerTask.description}</Typography>
+          <Typography>Status: {drawerTask.status}</Typography>
+        </Container>
+      </Drawer>
+    </div>
+  );
 };
 
 export default ProjectDetail;
