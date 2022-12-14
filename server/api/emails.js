8b7ff5fea79033aec8c1d999d410591e777eabfb
Ramir Salcedo <ramir101@gmail.com>
2022-12-13 19:12:28 -0500

    first commit

--- server/api/emails.js
+++ server/api/emails.js
@@ -32,3 +32,33 @@ app.post("/test", async (req, res, next) => {
     next(ex);
   }
 });
+
+app.post("/invite/team", async (req, res, next) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "Zoho",
+      auth: {
+        user: "devtest2207@zohomail.com",
+        pass: process.env.EMAILPASS,
+      },
+    });
+    const mailOptions = {
+      from: "devtest2207@zohomail.com",
+      to: "ramir101@gmail.com",
+      subject: "Sending Email using Node.js",
+      text: "second test",
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: 2 " + info.response);
+      }
+    });
+
+    res.sendStatus(201);
+  } catch (ex) {
+    next(ex);
+  }
+});
