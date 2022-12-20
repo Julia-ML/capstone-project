b394d711db488e9b90a0a2f4f5677d713d844871
Ethan Newlin <newlin5@gmail.com>
2022-12-20 10:48:23 -0500

    made updates to PDetail and TGallery

--- src/Components/TaskDelete.js
+++ src/Components/TaskDelete.js
@@ -32,9 +32,9 @@ const TaskDelete = (props) => {
 
 	return (
 		<Fragment>
-			<Tooltip title="Delete task">
+			<Tooltip title='Delete task'>
 				<IconButton>
-					<DeleteIcon onClick={handleClickOpen} />
+					<DeleteIcon onClick={handleClickOpen} sx={{ color: "#8b0000" }} />
 				</IconButton>
 			</Tooltip>
 			<Dialog open={open} onClose={handleClose}>
@@ -44,10 +44,10 @@ const TaskDelete = (props) => {
 					<DialogContentText>Description: {task.description}</DialogContentText>
 				</DialogContent>
 				<DialogActions>
-					<Button variant="contained" onClick={handleClose}>
+					<Button variant='contained' onClick={handleClose}>
 						Cancel
 					</Button>
-					<Button variant="contained" color={"error"} onClick={destroyTask}>
+					<Button variant='contained' color={"error"} onClick={destroyTask}>
 						Delete task
 					</Button>
 				</DialogActions>
