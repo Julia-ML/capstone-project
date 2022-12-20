e896db2b5565fbead798b706ebe13acfd0fdf982
Julia <juliam.sec@gmail.com>
2022-12-20 12:59:19 -0500

    landing page styling

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,32 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: 'babel-loader', 
         options: {
           presets: ['@babel/preset-react']
         }
-      }
+      },
+      {
+        test: /\.(jpg|jpeg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: './static/images/',
+          },
+        },
+      },
+      {
+        test: /\.(jpg|jpeg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            name: '[name].[ext]',
+            outputPath: './static/images/',
+          },
+        },
+      },
     ]
   }
 };
